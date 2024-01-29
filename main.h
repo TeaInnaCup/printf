4a328333480988c35639568762a903de8687458c
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-28 23:21:31 -0800

    header

--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-int _printf(const char *format, ...);
 
+int _printf(const char *format, ...);
+void handle_s(int *count, va_list args, int precision);
 #endif /* MAIN_H */

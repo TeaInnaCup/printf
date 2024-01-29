e239d82290ffd5d774cad43678833c3281770961
Motolani Chrsitianah Alebiosu <motolanialebiosu@gmail.com>
2024-01-29 12:56:34 -0800

    formatted output of s, c and %

--- form_output.c
+++ form_output.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include <string.h>
 
 /**
  * _printf - Custom printf function
@@ -13,58 +12,37 @@ int _printf(const char *format, ...)
 {
 	int count;
 	va_list args;
-	va_start(args, format);
+
 	count = 0;
+	va_start(args, format);
 
 	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
-			int precision;
-			precision = -1;
 			format++;
-
-			if (*format >= '0' && *format <= '9')
+			switch (*format)
 			{
-				precision = 0;
-				while (*format >= '0' && *format <= '9')
-				{
-					precision = precision * 10 + (*format - '0');
-					format++;
-				}
-			}
-			if (*format == 'r')
-			{
-				count += printf("%p", va_arg(args, void *));
-			}
-			else
-			{
-				/* Handle conversion specifiers */
-				switch (*format)
-				{
-					case 'c':
-						count += putchar(va_arg(args, int));
-						break;
-					case 's':
-						count += printf("%.*s", precision, va_arg(args, char *));
-						break;
-					case '%':
-						count += putchar('%');
-						break;
-						/* more cases for additional conversion with specifiers */
-					default:
-						count += putchar('%');
-						count += putchar(*format);
-						break;
-				}
+				case 'c':
+					count += putchar(va_arg(args, int));
+					break;
+				case 's':
+					count += printf("%s", va_arg(args, char *));
+					break;
+				case '%':
+					count += putchar('%');
+					break;
+				default:
+					fprintf(stderr, "Unknown conversion specifier: %%%c\n", *format);
+					break;
 			}
 		}
-			else
-			{
-				count += putchar(*format);
-			}
-			format++;
+		else
+		{
+			count += putchar(*format);
 		}
-		va_end(args);
-		return (count);
+		format++;
+	}
+	va_end(args);
+	return (count);
 }

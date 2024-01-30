8413c355b010adeebae2e11b9cf4b8da60072621
LyrealCurriculum <lyreal.real@gmail.com>
2024-01-30 20:08:43 +0100

    Update to main.h

--- main.h
+++ main.h
@@ -3,22 +3,13 @@
 
 #include <stdio.h>
 #include <stdarg.h>
-<<<<<<< HEAD
 #include <unistd.h>
 #include <limits.h>
 
 int _putchar(char character);
 int printf_char(va_list args, int printed);
 int printf_string(va_list args, int printed);
-int printf_integer(va_list args, int printed);
 int selector(const char *format, va_list args, int printed);
-int printf_binary(unsigned int num, int printed);
 int _printf(const char *format, ...);
-int _x(unsigned int num, int printed, int uppercase);
-int printf_octal(unsigned int num, int printed);
-int printf_unsigned(unsigned int num, int printed);
-int printf_reverse(va_list args, int printed);
-int printf_pointer(va_list args, int printed);
-void handle_s(int *count, va_list args, int precision);
 
 #endif

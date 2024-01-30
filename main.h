b5403ef456b344ac584e15e174dec9758495308d
LyrealCurriculum <lyreal.real@gmail.com>
2024-01-30 20:26:16 +0100

    Update to main.h

--- main.h
+++ main.h
@@ -9,7 +9,14 @@
 int _putchar(char character);
 int printf_char(va_list args, int printed);
 int printf_string(va_list args, int printed);
+int printf_integer(va_list args, int printed);
 int selector(const char *format, va_list args, int printed);
+int printf_binary(unsigned int num, int printed);
 int _printf(const char *format, ...);
+int _x(unsigned int num, int printed, int uppercase);
+int printf_octal(unsigned int num, int printed);
+int printf_unsigned(unsigned int num, int printed);
+int printf_reverse(va_list args, int printed);
+int printf_pointer(va_list args, int printed);
 
 #endif

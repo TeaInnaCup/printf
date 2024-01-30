a149eb7325a2a8c5cb921eac1072448a450afcdd
LyrealCurriculum <lyreal.real@gmail.com>
2024-01-30 20:22:49 +0100

    c, s and % specifiers for printf()

--- 0-printf_character_string.c
+++ 0-printf_character_string.c
@@ -1,86 +1,36 @@
 #include "main.h"
 
 /**
- * _printf - outputs specified format
- * @format: the format specifier
- * Return: the formated string
+ * printf_char - handles c specifier
+ * @args: no. of arguments
+ * @printed: printed characters
+ * Return: printed characters
  */
 
-int _printf(const char *format, ...)
+int printf_char(va_list args, int printed)
 {
-	int printed = 0;
+	int character = va_arg(args, int);
 
-	va_list args;
-
-	va_start(args, format);
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			printed = selector(format, args, printed);
-			format++;
-		}
-		else
-		{
-			_putchar(*format);
-			printed++;
-			format++;
-		}
-	}
-	va_end(args);
-	return (printed);
+	_putchar(character);
+	return (printed + 1);
 }
 
 /**
- * selector - selects format specifiers
- * @args: no. of arguments
- * @printed: the printed characters
- * @format: the format specifier
- * Return: printed charcaters
+ * printf_string - handles s specifier
+ * @args: no. of arguements
+ * @printed: printed characters
+ * Return: printed characters
  */
 
-int selector(const char *format, va_list args, int printed)
+int printf_string(va_list args, int printed)
 {
-	switch (*format)
+	char *string = va_arg(args, char *);
+
+	while (*string != '\0')
 	{
-		case 'd':
-		case 'i':
-			printed = printf_integer(args, printed);
-			break;
-		case 'c':
-			_putchar(va_arg(args, int));
-			printed++;
-			break;
-		case 's':
-			printed = printf_string(args, printed);
-			break;
-		case '%':
-			_putchar('%');
-			printed++;
-			break;
-		case 'b':
-			printed = printf_binary(va_arg(args, unsigned int), printed);
-			break;
-		case 'x':
-		case 'X':
-			printed = _x(va_arg(args, unsigned int), printed, (*format == 'X') ? 1 : 0);
-			break;
-		case 'o':
-			printed = printf_octal(va_arg(args, unsigned int), printed);
-			break;
-		case 'u':
-			printed = printf_unsigned(va_arg(args, unsigned int), printed);
-			break;
-		case 'r':
-			printed = printf_reverse(args, printed);
-			break;
-		case 'p':
-			printed = printf_pointer(args, printed);
-			break;
-		default:
-			break;
+		_putchar(*string);
+		printed++;
+		string++;
 	}
 	return (printed);
 }
